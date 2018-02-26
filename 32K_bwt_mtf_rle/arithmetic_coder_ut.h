e7143786471880ab71592025af9fc6dcd32c3491
Michael S <already5chosen@yahoo.com>
2018-02-27 01:50:17 +0200

    32k_bwt_mtf_rle - try different method of encoding of the model
    So far results are inferior to previous method

--- 32K_bwt_mtf_rle/arithmetic_coder_ut.h
+++ 32K_bwt_mtf_rle/arithmetic_coder_ut.h
@@ -1,3 +1,6 @@
 #include <cstdint>
 
 void quantized_histogram_to_range(uint16_t* c2range, unsigned len, const uint8_t* qh, unsigned range_scale);
+unsigned quantized_histogram_pair_to_range_qh_scale7(unsigned qh, unsigned range_scale);
+unsigned quantized_histogram_pair_to_range_qh_scale8(unsigned qh, unsigned range_scale);
+unsigned quantized_histogram_pair_to_range_qh_scale9(unsigned qh, unsigned range_scale);
